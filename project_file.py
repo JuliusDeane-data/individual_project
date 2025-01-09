a892e96c16fe722c82e38b872728fcfbca87ffb8
Philip Koloczek <koloczek@gmail.com>
2025-01-09 11:49:50 +0100

    Player can choose number of rounds. Can't choose same question twice.

--- project_file.py
+++ project_file.py
@@ -1,9 +1,11 @@
 # The Quiz Game
+
 """ 
 - Player inputs name
-- Player answers 5 questions
+- Player answers between 1 and 20 questions
 - player gets score
 """
+
 questions = {
     1: {
         "question": "What is the standard file extension for Python files?",
@@ -108,46 +110,62 @@ questions = {
 }
 
 player = input("What's your name? ")
+rounds = int(input("How many rounds do you want to play? (1-20) "))
+score = 0 
+questions_picked = []
 
 def show_score(player_name = 'Unknown', points = 0):
     """Shows the final score to the player"""
-    if points == 5:
-        return f'Amazing job {player_name}! A perfect 5/5 score, are you Linus Torwalds or Chris Sawyer?'
-    elif points < 5 and points >= 3:
-        return f'Good job {player_name}! A {points}/5 score is nothing to sneeze at.'
-    elif points < 3 and points >= 1:
-        return f'You can do better {player_name}! With a {points}/5 score you have a lot of room for improvement.'       
+    if points == rounds:
+        return f'Amazing job {player_name}! A perfect {points}/{rounds} score, are you Linus Torwalds or Chris Sawyer?'
+    elif points < rounds and points >= (rounds/2):
+        return f'Good job {player_name}! A {points}/{rounds} score is nothing to sneeze at.'
+    elif points < (rounds/2) and points >= 1:
+        return f'You can do better {player_name}! With a {points}/{rounds} score you have a lot of room for improvement.'       
     else:
-        return f'Are you okay {player_name}? A {points}/5 score is not so good. Please see a doctor.'
+        return f'Are you okay {player_name}? A {points}/{rounds} score is not so good. Please see a doctor.'
 
 def show_question(number_of_question):
     """returns the question and answer options to the user"""
-    print(f'{questions[number_of_question]["question"]}\n{"  ".join(questions[number_of_question]["options"])}')
+    print(f'\n\n{questions[number_of_question]["question"]}\n\n{"  ".join(questions[number_of_question]["options"])}')
     
 
 def check_answer(player_answer, number_of_question):
     """checks if given answer is correct"""
     if player_answer == questions[number_of_question]['answer']:
-        print(f'Correct! You get +1 point')
+        print(f'\nCorrect! You get +1 point')
         return 1
     else:
-        print(f"Sorry, you're wrong. The correct answer is {questions[number_of_question]['answer']}")
+        print(f"\nSorry, you're wrong. The correct answer is {questions[number_of_question]['answer']}")
         return 0
+
+def pick_question():
+    """Let's player choose a question. If question was chosen before, it makes them choose another question"""
+    number = int(input(f"\n{player} pick a number betweeen 1 - 20 to get your next question: "))
+    if number in questions_picked:
+        print(f'You already answered question {number}. Please pick a different question.')
+        number = pick_question()
+    else:
+        questions_picked.append(number)
+        show_question(number)
+    return number
+
 
 def main_quiz(name, score):
     """present questions to the player, check answers, track score"""
     print(f"Your current score is {score}.")
-    question_number = int(input(f"{name} pick a number betweeen 1 - 20 to get your next question: "))
-    show_question(question_number)
-    answer_choice = input(f"Please choose your answer (a, b, c, d): ")
+    question_number = pick_question()
+    #question_number = int(input(f"{name} pick a number betweeen 1 - 20 to get your next question: "))
+    #show_question(question_number)
+    answer_choice = input(f"\nPlease choose your answer (a, b, c, d): ")
     score = check_answer(answer_choice, question_number)
     return score
-    
-score = 0   
+
 n = 0
-while n < 5:
+
+while n < rounds:
     score += main_quiz(player, score)
     n += 1
 
-print(show_score(player, score))
+print(f'\n {show_score(player, score)}')
 
